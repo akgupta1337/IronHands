632bb6969116c80a59d5c69cbf070358acc02bd5
Akhil Kumar Gupta <akgupta1337@gmail.com>
2024-05-08 21:29:58 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Iron Hands: Gesture-controlled 3D Object Manipulation in Blender
+![Iron Man](https://gifdb.com/images/high/tony-stark-fabulous-blueprint-n8vfiy5nkjpyglpr.gif)
+## Overview
+Iron Hands is an exciting project inspired by the iconic hand gestures from the Iron Man movies. It allows users to control and manipulate 3D objects in Blender using simple hand gestures detected by a webcam. Whether you're a fan of Tony Stark or just fascinated by the idea of interacting with virtual objects like a superhero, Iron Hands brings that fantasy to life!
+
+## Features
+- **Gesture Control**: Perform hand gestures to rotate, scale, and move 3D objects in Blender.
+- **Intuitive Interface**: No need for complex controls or input devices – simply use your hands!
+- **Real-time Feedback**: See immediate visual feedback as you perform gestures, just like in the movies.
+- **Customizable Configurations**: Tailor the project to your preferences with configurable parameters.
+
+## Video Preview
+[![Iron Hands Demo](https://img.youtube.com/vi/T73HS1iFST4/0.jpg)](https://www.youtube.com/shorts/T73HS1iFST4)
+
+## Getting Started
+1. **Requirements**: Make sure you have Python 3.x, OpenCV, Mediapipe, and Blender installed on your system.
+2. **Installation**: Clone or download this repository and install the required dependencies using `pip install -r requirements.txt`.
+3. **Run the Application**: Execute the `main.py` script to launch the Iron Hands application.
+4. **Setup Blender**: Open the `arc_reactor_lab.blend` file in Blender, and run the blender script(don't forget to update the blender script with new file location).
+5. **Have Fun!**: Start experimenting with different hand gestures to control the 3D objects in Blender and unleash your inner superhero.
+
+## About the Project
+Iron Hands was born out of a combination of boredom and inspiration during the winter holidays of the first semester in college. It's a testament to the power of creativity and technology coming together to create something truly enjoyable and memorable. Whether you're a student looking for a fun project or a fan of Iron Man seeking to recreate the magic of Tony Stark's technology, Iron Hands promises hours of entertainment and excitement.
+
+
+
+## License
+This project is licensed under the MIT License.
+
+## Feedback and Contributions
+We welcome any feedback, suggestions, or contributions to make Iron Hands even better! Feel free to open an issue or submit a pull request on GitHub.
+
+Let's bring the magic of Iron Man's technology to life – one hand gesture at a time!
---
+++ main.py
@@ -0,0 +1,273 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+from HandTrackingModule import HandDetector
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands
+
+#intialize files
+with open("scale.txt", "w") as file:
+    file.write("")
+
+with open("pan.txt", "w") as file:
+    file.write("0 0")
+
+with open("rotate.txt", "w") as file:
+    file.write("0")
+
+#constants
+scale_factor = 0
+min_length = 18.439088914585774
+max_length = 200.5356303021603
+min_scale = 10143
+max_scale = 71189
+x_blend = 15
+z_blend = 8
+min_s_blend = 0.002
+max_s_blend = 0.04
+rd = 1
+
+
+# Open the webcam
+cap = cv2.VideoCapture(0)
+
+# Create a HandDetector instance
+detector = HandDetector(maxHands=2, detectionCon=0.9)
+
+with mp_hands.Hands(
+    model_complexity=0, min_detection_confidence=0.9, min_tracking_confidence=0.9
+) as hands:
+    while True:
+        ret, frame = cap.read()
+        # Flip the frame horizontally for a later selfie-view display
+        # Detect hands in the frame
+        frame = cv2.flip(frame, 1)
+        hands, frame = detector.findHands(frame, draw=True)
+        # Check if at least one hand is detected
+        if hands:  # single
+            if len(hands) == 1:
+                hand1 = hands[0]  # Get the first hand detected
+                lmList1 = hand1["lmList"]  # List of 21 landmarks for the first hand
+                bbox1 = hand1[
+                    "bbox"
+                ]  # Bounding box around the first hand (x,y,w,h coordinates)
+                handType1 = hand1["type"]
+                fingerup1 = detector.fingersUp(hand1)  # TODO: scale, rotate
+                area = bbox1[2] * bbox1[3]
+                center1 = hand1["center"]
+                x, z = center1[0], center1[1]
+                xc = lmList1[8][0]
+
+                fingerup1 = detector.fingersUp(hand1)
+                fist = detector.fist(hand1)
+
+                if handType1 == "Left":  # rotate and scale
+                    if fingerup1 == [1, 1, 0, 0, 0]:  # rotate
+                        length, _, frame = detector.findDistance(
+                            lmList1[4][0:2], lmList1[8][0:2], frame
+                        )
+                        rotate = np.interp(length, [min_length, max_length], [-rd, rd])
+                        with open("rotate.txt", "w") as file:
+                            file.write(str(float(rotate)))
+
+                        cv2.putText(
+                            frame,
+                            f"Rotate Factor: {float(rotate)}",
+                            (10, 40),
+                            cv2.FONT_HERSHEY_SIMPLEX,
+                            0.7,
+                            (0, 255, 0),
+                            2,
+                        )
+
+                        cv2.putText(
+                            frame,
+                            f"X , Z: {float(x)},{float(z)}",
+                            (10, 70),
+                            cv2.FONT_HERSHEY_SIMPLEX,
+                            0.7,
+                            (0, 0, 255),
+                            2,
+                        )
+                    elif fingerup1 != [1, 1, 0, 0, 0]:  # scale
+                        with open("rotate.txt", "w") as file:
+                            file.write(str(float(0)))
+                        scale_factor = np.interp(
+                            area, [min_scale, max_scale], [min_s_blend, max_s_blend]
+                        )
+                        scale_factor = "{:.5f}".format(scale_factor)
+                        with open("scale.txt", "w") as file:
+                            file.write(str(float(scale_factor)))
+
+                        cv2.putText(
+                            frame,
+                            f"Scale Factor: {float(scale_factor)}",
+                            (10, 40),
+                            cv2.FONT_HERSHEY_SIMPLEX,
+                            0.7,
+                            (255, 0, 0),
+                            2,
+                        )
+                else:
+                    if fist and fingerup1 != [0, 1, 0, 0, 0]:  # pan
+                        with open("rotate.txt", "w") as file:
+                            file.write("0")
+                        x = np.interp(x, [69, 527], [-x_blend, x_blend])
+                        z = np.interp(z, [69, 412], [z_blend, -z_blend])
+                        x = "{:.5f}".format(x)
+                        z = "{:.5f}".format(z)
+                        with open("pan.txt", "w") as file:
+                            file.write(str(float(x)))
+                            file.write(str(" "))
+                            file.write(str(float(z)))
+
+                        cv2.putText(
+                            frame,
+                            f"X , Z: {float(x)},{float(z)}",
+                            (10, 70),
+                            cv2.FONT_HERSHEY_SIMPLEX,
+                            0.7,
+                            (0, 0, 255),
+                            2,
+                        )
+
+                    elif fingerup1 == [0, 1, 0, 0, 0]:  # stop rotate
+                        with open("rotate.txt", "w") as file:
+                            file.write("0")
+                        cv2.putText(
+                            frame,
+                            "POP",
+                            (10, 40),
+                            cv2.FONT_HERSHEY_SIMPLEX,
+                            0.7,
+                            (180, 200, 70),
+                            2,
+                        )
+
+                    elif fingerup1 == [0, 1, 1, 1, 1]:  # rotate
+                        rotate = np.interp(xc, [56, 537], [-rd, rd])
+                        with open("rotate.txt", "w") as file:
+                            file.write(str(float(rotate)))
+
+                        cv2.putText(
+                            frame,
+                            f"Rotate Factor: {float(rotate)}",
+                            (10, 40),
+                            cv2.FONT_HERSHEY_SIMPLEX,
+                            0.7,
+                            (0, 255, 0),
+                            2,
+                        )
+            else:
+                hand1 = hands[0]  # Get the new hand detected
+                handType1 = hand1["type"]
+                if handType1 == "Right":
+                    i = 0
+                    j = 1
+                else:
+                    i = 1
+                    j = 0
+                hand1 = hands[i]  # Get the new hand detected
+                lmList1 = hand1["lmList"]  # List of 21 landmarks for the first hand
+                handType1 = hand1["type"]
+                center1 = hand1["center"]
+                fingerup1 = detector.fingersUp(hand1)
+                area = bbox1[2] * bbox1[3]
+                fingerup1 = detector.fingersUp(hand1)
+
+                hand2 = hands[j]
+                fingerup2 = detector.fingersUp(hand2)  # TODO: scale+pan, rotate+pan
+                lmList2 = hand2["lmList"]
+                handType2 = hand2["type"]
+                x, z = center1[0], center1[1]
+                bbox1 = hand2["bbox"]
+                fist = detector.fist(hand1)
+
+                if handType1 == "Right" and fist:
+                    with open("rotate.txt", "w") as file:
+                        file.write(str(float(0)))
+                    x = np.interp(x, [63, 404], [-x_blend, x_blend])
+                    z = np.interp(z, [64, 434], [z_blend, -z_blend])  # pan
+                    x = "{:.5f}".format(x)
+                    z = "{:.5f}".format(z)
+
+                    with open("pan.txt", "w") as file:
+                        file.write(str(float(x)))
+                        file.write(str(" "))
+                        file.write(str(float(z)))
+
+                    cv2.putText(
+                        frame,
+                        f"X , Z: {float(x)},{float(z)}",
+                        (10, 70),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        0.7,
+                        (0, 0, 255),
+                        2,
+                    )
+
+                if (
+                    handType1 == "Right"
+                    and fingerup1 == [1, 1, 0, 0, 0]
+                    and fingerup2 != [1, 1, 0, 0, 0]
+                ):
+                    length, _, img = detector.findDistance(
+                        lmList1[4][0:2], lmList1[8][0:2], frame
+                    )
+                    rotate = np.interp(length, [min_length, max_length], [-rd, rd])
+                    with open("rotate.txt", "w") as file:
+                        file.write(str(float(rotate)))  # rotate from pan
+
+                    cv2.putText(
+                        frame,
+                        f"Rotate Factor: {float(rotate)}",
+                        (10, 100),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        0.7,
+                        (0, 255, 0),
+                        2,
+                    )
+
+                if fingerup2 == [1, 1, 0, 0, 0]:  # rotate
+                    length, _, img = detector.findDistance(
+                        lmList2[4][0:2], lmList2[8][0:2], frame
+                    )
+                    rotate = np.interp(length, [min_length, max_length], [-rd, rd])
+                    with open("rotate.txt", "w") as file:
+                        file.write(str(float(rotate)))  # normal rotate
+
+                    cv2.putText(
+                        frame,
+                        f"Rotate Factor: {float(rotate)}",
+                        (10, 40),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        0.7,
+                        (0, 255, 0),
+                        2,
+                    )
+
+                else:
+                    scale_factor = np.interp(
+                        area, [min_scale, max_scale], [min_s_blend, max_s_blend]
+                    )
+                    with open("scale.txt", "w") as file:
+                        file.write(str(float(scale_factor)))
+
+                    cv2.putText(
+                        frame,
+                        f"Scale Factor: {float(scale_factor)}",
+                        (10, 40),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        0.7,
+                        (255, 0, 0),
+                        2,
+                    )
+
+        cv2.imshow("GOD", frame)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+cap.release()
+cv2.destroyAllWindows()
